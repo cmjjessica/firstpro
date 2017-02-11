9d0c8c2699bbded04466ffef98f8fb38d2378d7b
陈敏杰 <xxx@xx.com>
2017-02-11 17:44:53 +0800

    cccc

---
+++ MyApplication/app/src/main/java/com/example/jessica/mypro1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.jessica.mypro1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void metho1(){
+        System.out.println("addmethod1111111");
+    }
+}
